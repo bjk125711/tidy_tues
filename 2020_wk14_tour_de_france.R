80a14fcb406f701e12f4f08eba4ca5c32c99c970
bjk125711 <bennettkellogg@gmail.com>
2020-04-19 22:07:24 -0700

    Added more graphs

--- 2020_wk14_tour_de_france.R
+++ 2020_wk14_tour_de_france.R
@@ -60,7 +60,7 @@ table(tdf_winners$winner_team)
 
 # Who is the winningest team?
 # France is. 
-# The USPS had a team?? No was Lance Armstrong's team
+# The USPS had a team?? Yes, was Lance Armstrong's team.
 tdf_winners %>% 
   group_by(winner_team) %>% 
   summarize(n = n()) %>% 
@@ -74,3 +74,47 @@ tdf_winners %>%
   ylab("Count Wins") +
   ggtitle("Top 10 Winningest Teams") +
   scale_y_continuous(breaks = c(0:13))
+
+# How has distance of the race changed over time?
+# Interestingly the TDF has gotten shorter over time. How has that corresponded with rider speed?
+tdf_winners %>% 
+  ggplot() +
+  geom_point(aes(race_year, distance, color = edition)) +
+  xlab("Year") +
+  ylab("Distance (KM)") +
+  ggtitle("Distance of the TDF over time") +
+  theme(legend.position = 'none')
+
+# The races may be shorter but they are MUCH faster
+tdf_winners %>% 
+  mutate(
+    avg_speed = distance / time_overall 
+  ) %>% 
+  ggplot() +
+  geom_point(aes(race_year, avg_speed, color = edition)) +
+  xlab("Year") +
+  ylab("Avg KM / H") +
+  ggtitle("Average Speed of TDF Winners Over Time") +
+  theme(legend.position = 'none')
+
+# Are riders smaller than they used to be?
+tdf_winners %>%  # no clear pattern
+  ggplot() +
+  geom_point(aes(race_year, height))
+
+tdf_winners %>%  # No clear patter
+  ggplot() +
+  geom_point(aes(race_year, weight))
+
+tdf_winners %>%  # Maybe a pattern here - smaller BMI over time?
+  ggplot() +
+  geom_point(aes(race_year, weight / height, color = edition))
+
+# Has the competitiveness of the race increased or decreased over time?
+# The tour has become much more competitive over time with margin of victory shrinking from nearly three hours to 8 seconds
+tdf_winners %>% 
+  ggplot() +
+  geom_point(aes(race_year, time_margin, color = edition)) +
+  xlab("Year") +
+  ylab("Winning")
+
